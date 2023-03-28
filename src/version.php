d72746f9ba55a3627c91c0a53dffc0e0863eb13b
Gustavo Bazzo <gcbazzo@gmail.com>
2023-03-28 16:02:51 -0300

    Update version.php

--- src/version.php
+++ src/version.php
@@ -24,12 +24,12 @@
  
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023012500;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2023032800;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2020061500.00; // Moodle 3.9.0
 $plugin->component = 'atto_htmlbootstrapeditor';  // Full name of the plugin (used for diagnostics).
-$plugin->release = 'v1.18.1-stable';
+$plugin->release = 'v1.0.0-beta';
 $plugin->supported = [39, 410];      //  Moodle 3.9.x, 3.10.x and 3.11.x are supported.
-$plugin->maturity = MATURITY_STABLE; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
+$plugin->maturity = MATURITY_BETA; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
 $plugin->dependencies = [
-	'tool_htmlbootstrapeditor' => 2021013100
+	'tool_htmlbootstrapeditor' => 2023032800
 ];
