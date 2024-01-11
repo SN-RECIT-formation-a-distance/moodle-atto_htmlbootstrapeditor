06688b5a34b5c614af3798666a59a4c72bd98964
Gustavo Bazzo <gcbazzo@gmail.com>
2024-01-11 14:43:31 -0500

    Fix #2, #3

--- src/lang/en/atto_htmlbootstrapeditor.php
+++ src/lang/en/atto_htmlbootstrapeditor.php
@@ -2,4 +2,3 @@
 $string['pluginname'] = 'HTML Bootstrap Editor Atto';
 $string['htmleditor'] = 'HTML Bootstrap Editor';
 $string['privacy:metadata'] = 'The atto_htmlbootstrapeditor plugin does not store user data.';
-?>
