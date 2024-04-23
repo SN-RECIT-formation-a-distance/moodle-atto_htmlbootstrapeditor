d049f8fb3c7e3e809400163689488f689cd8f988
mitchell-accipio <mitchell@accipio.com>
2024-04-23 17:52:55 +0100

    adjusted folder structure to moodle standard

--- sync.ps1
+++
@@ -1,9 +0,0 @@
-$from = "moodle-atto_htmlbootstrapeditor/src/*"
-$to = "shared/recitfad/lib/editor/atto/plugins/htmlbootstrapeditor"
-
-try {
-    . ("..\sync\watcher.ps1")
-}
-catch {
-    Write-Host "Error while loading sync.ps1 script." 
-}
